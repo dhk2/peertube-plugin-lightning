0595929613ca41b9650aa19ff01e66b4c666031a
Chocobozzz <me@florianbigard.com>
2021-08-11 11:32:25 +0200

    Update to webpack 5

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -1,5 +1,5 @@
 function register ({ registerHook, peertubeHelpers }) {
-
+  console.log('Hello world')
 }
 
 export {
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
 const path = require("path")
 
-const EsmWebpackPlugin = require("@purtuga/esm-webpack-plugin")
-
 const clientFiles = [
   'common-client-plugin.js'
 ]
 
 let config = clientFiles.map(f => ({
   entry: "./client/" + f,
+
+  experiments: {
+    outputModule: true
+  },
+
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "./" + f,
-    library: "script",
-    libraryTarget: "var"
-  },
-  plugins: [ new EsmWebpackPlugin() ]
+    library: {
+      type: "module"
+    }
+  }
 }))
 
 module.exports = config
