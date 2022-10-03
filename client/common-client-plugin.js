83be3969b5bfd89c3dd3920f4a6204a6641de5bf
dhk2 <donkimberlin@hotmail.com>
2022-10-03 09:29:24 +0000

    first milestone reached and boosted

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -1,7 +1,137 @@
-function register ({ registerHook, peertubeHelpers }) {
-  console.log('Hello world')
-}
+import axios from 'axios';
+async function register({ registerHook, peertubeHelpers }) {
+  console.log('Hello third world');
+  registerHook({
+    target: 'action:router.navigation-end',
+    handler: async ({ path }) => {
+      const panel = document.createElement('div');
+      panel.setAttribute('class', 'lightning-button');
+      panel.setAttribute('sandbox', 'allow-same-origin allow-scripts allow-popups allow-forms')
+      const html = `
+      <label for="message">message:</label><br>
+      <input type="text" id="message" name="message" maxLength="128"><br><br>
+      <input type="text" id="sats" name="sats" maxLength="8">
+      <label for="sats"> Sats</label><br>
+      <peertube-button data-href="https://trutube.online/signup" data-theme="primary" data-label="Login with Telegram">
+      <div _nghost-cav-c140="" ng-version="13.3.0">
+        <p id="satbutton" onclick="()">⚡️Send Sats⚡️</p>
+        </peertube-button>
 
+      </div>`
+
+      panel.innerHTML = html;
+
+      setInterval(async function () {
+        if ((document.querySelector('.top-menu .lightning-button') === null)) {
+          const topMenu = document.querySelector('.top-menu');
+          console.log("topmenu", topMenu);
+          if (topMenu) {
+            console.log("insterting panel into topmenu", panel)
+            topMenu.appendChild(panel);
+          }
+
+          document.getElementById("satbutton").onclick = function () {
+            let amount = document.getElementById('sats').value
+            let message = document.getElementById('message').value
+            SendSats(amount, message)
+          };
+
+        }
+        if ((document.querySelector('.menu-wrapper .lightning-button') === null)) {
+          const mainContent = document.querySelector('.menu-wrapper');
+          console.log("maincontent", mainContent)
+          if (mainContent) {
+            panel.classList.add('section')
+            mainContent.appendChild(panel)
+            console.log("Panel added to main content", panel);
+          }
+        }
+      }, 1)
+    }
+  })
+  async function SendSats(amount, message) {
+    if (!message) {
+      message = ":)";
+    }
+    if (!parseInt(amount)) {
+      amount = "69";
+    }
+    console.log("parsint", parseInt(amount));
+    if (parseInt(amount) < 1) {
+      amount = "69";
+    }
+    await webln.enable();
+    console.log("---------------\n", "webln enabled");
+    let walletData = await axios.get("https://lawsplaining.peertube.biz/plugins/lightning/router/walletinfo");
+    console.log(walletData.data);
+
+    let pubKey = walletData.data.pubkey;
+    let tag = walletData.data.tag;
+    let customKey = walletData.data.customData[0].customKey;
+    let customValue = walletData.data.customData[0].customValue;
+    let paymentInfo = {
+      destination: pubKey,
+      amount: amount,
+      customRecords: {
+        "34349334": message,
+        "696969": customValue,
+      }
+    }
+
+
+    console.log("payment info", paymentInfo);
+    console.log("parts", paymentInfo.destination, paymentInfo.amount);
+    console.log("custom records", paymentInfo.customRecords);
+    await webln.keysend(paymentInfo);
+  }
+  async function boost(amount, message, from) {
+    if (!message) {
+      message = ":)";
+    }
+    if (!parseInt(amount)) {
+      amount = "69";
+    }
+    console.log("parsint", parseInt(amount));
+    if (parseInt(amount) < 1) {
+      amount = "69";
+    }
+    if (!from) {
+      from = "Anon";
+    }
+    await webln.enable();
+    console.log("---------------\n", "webln enabled");
+    let walletData = await axios.get("https://lawsplaining.peertube.biz/plugins/lightning/router/walletinfo");
+    console.log(walletData.data);
+
+    let pubKey = walletData.data.pubkey;
+    let tag = walletData.data.tag;
+    let customKey = walletData.data.customData[0].customKey;
+    let customValue = walletData.data.customData[0].customValue;
+    const boost = {
+      696969: customValue,
+      action: "boost",
+      value_msat: 1000,
+      value_msat_total: 1000,
+      app_name: "PeerTube",
+      app_version: "1.0",
+      name: "PeerTube",
+      sender_name: "PeerTube",
+      message: message
+    };
+    let paymentInfo = {
+      destination: pubKey,
+      amount: amount,
+      customRecords: {
+        7629169: boost,
+        696969: customValue,
+      }
+    };
+    console.log("payment info", paymentInfo);
+    console.log("parts", paymentInfo.destination, paymentInfo.amount);
+    console.log("custom records", paymentInfo.customRecords);
+    await webln.keysend(paymentInfo);
+  }
+}
 export {
   register
 }
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 async function register ({
   registerHook,
   registerSetting,
@@ -9,7 +11,70 @@ async function register ({
   videoLicenceManager,
   videoLanguageManager
 }) {
+/*
+  registerSetting({
+    name: 'lightning-custom-key',
+    label: 'Custom Key',
+    type: 'input',
+    private: true
+  })
 
+  registerSetting({
+    name: 'lightning-custom-value',
+    label: 'Custom Value',
+    type: 'input',
+    private: true
+  })
+
+  registerSetting({
+    name: 'lightning-node-address',
+    label: 'Node Address',
+    type: 'input',
+    private: true
+  })
+*/
+  registerSetting({
+    name: 'lightning-address',
+    label: 'Lightning address',
+    type: 'input',
+    private: true
+  })
+  let lightningAddress = await settingsManager.getSetting("lightning-address");
+  let walletHost = lightningAddress.substring(lightningAddress.indexOf('@') + 1);
+  let walletUser = lightningAddress.substring(0, lightningAddress.indexOf('@'));
+  console.log("-------------------\n wallet host:",walletHost,"\n wallet user",walletUser);
+  let apiRequest = "https://"+walletHost+"/.well-known/keysend/"+walletUser
+  let walletData = await axios.get(apiRequest)
+  //console.log("-------------------\n Api data:",walletData);
+  if (walletData.data.status != "OK"){
+    console.log("------------------ \n Error getting lightning address data");
+    return;
+  }
+  let pubKey = walletData.data.pubkey;
+  let tag = walletData.data.tag;
+  let customKey = walletData.data.customData[0].customKey;
+  let customValue = walletData.data.customData[0].customValue;
+  const router = getRouter();
+  router.use('/tip', async (req, res) => {
+    
+    var tipHtml = "<html>\n<head>\n";
+    tipHtml = tipHtml +'<meta name=”lightning” content=”lnurlp:”'+lightningAddress+'/>';
+    tipHtml = tipHtml +'<meta property=”og:image” content=”https://commons.wikimedia.org/wiki/File:Logopeertube.png” /> '
+    tipHtml = tipHtml +"</head>\n";
+    tipHtml = tipHtml + "<body>key "+pubKey+"<br>tag "+tag+"<br> custom key "+customKey+"<br> custom value "+customValue;
+    tipHtml
+    tipHtml =tipHtml+"\n</body>\n</html>";
+    return res.status(200).send(tipHtml);
+
+  })
+
+  router.use('/walletinfo', async (req, res) => {
+    
+
+    return res.status(200).send(walletData.data);
+
+  })
+  
 }
 
 async function unregister () {
